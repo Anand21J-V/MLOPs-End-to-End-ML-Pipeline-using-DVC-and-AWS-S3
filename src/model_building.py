1e9c269497507a490034dca0a340dbcf2c51e3a5
ANAND VISHWAKARMA <anandvishwakarma21j@gmail.com>
2025-04-03 23:25:54 +0530

    params configured for the pipeline using DVC and Yaml

--- src/model_building.py
+++ src/model_building.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 import logging
 from sklearn.ensemble import RandomForestClassifier
-#import yaml
+import yaml
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -29,7 +29,6 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 
-'''
 def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
     try:
@@ -46,7 +45,7 @@ def load_params(params_path: str) -> dict:
     except Exception as e:
         logger.error('Unexpected error: %s', e)
         raise
-'''
+
 
 
 
@@ -123,8 +122,8 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        #params = load_params('params.yaml')['model_building']
-        params = {'n_estimators':25,'random_state':2}
+        params = load_params('params.yaml')['model_building']
+        #params = {'n_estimators':25,'random_state':2}
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values 
